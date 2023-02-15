21ef30df27ad2bdad853381738b2ba7a90ae6618
hnapv <vuphucanh1197@gmail.com>
2023-02-15 22:34:58 +0700

    convert to useState

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,50 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DisplayInfo.scss"
 import logo from "../logo.svg"
 
-class DisplayInfo extends React.Component {
-    state = {
-        isShowListUser: true
-    }
-    handleShowHide = () => {
-        this.setState({
-            isShowListUser: !this.state.isShowListUser
-        })
-    }
-    handleOnClick = () => {
-        alert("me")
+
+// stateless & stateful
+// class DisplayInfo extends React.Component {
+
+//     handleOnClick = () => {
+//         alert("me")
+//     }
+
+//     // 1 component = template + logic js
+//     render() {
+//         console.log("render")
+//         //prop 
+//         const { listUsers } = this.props
+//         return (
+//             <div className="display-info-container">
+//                 {true &&
+//                     <>
+//                         {listUsers.map(a => {
+//                             console.log(a)
+//                             return (
+//                                 <div className={+a.age > 18 ? "red" : "green"} key={a.id}>
+//                                     <div> Hello {a.name}</div>
+//                                     <div> I'm {a.age}</div>
+//                                     <button onClick={() => this.props.handleDeleteUser(a.id)} >Delete</button>
+//                                     <hr />
+//                                 </div>
+//                             )
+//                         })}
+
+//                     </>
+//                 }
+//                 {/* <img src={logo} alt="logo thôi mà"/> */}
+//             </div>
+//         )
+//     }
+// }
+
+const DisplayInfo = (props) => {
+    const { listUsers } = props
+
+    const [isShowHideListUser,setIsShowHideListUser] = useState(true)
+    //destructuring assignment
+
+    const handleShowHideListUser = ()=>{
+        setIsShowHideListUser(!isShowHideListUser)
     }
 
-    // 1 component = template + logic js
-    render() {
-        //prop 
-        const { listUsers } = this.props
-        return (
-            <div className="display-info-container">
-                <div>
-                    <button onClick={() => { this.handleShowHide() }}>
-                        {this.state.isShowListUser === true ? "Hide" : "Show"}</button>
-                </div>
-                {this.state.isShowListUser &&
-                    <>
-                        {listUsers.map(a => {
-                            console.log(a)
-                            return (
-                                <div className={+a.age > 18 ? "red" : "green"} key={a.id}>
-                                    <div> Hello {a.name}</div>
-                                    <div> I'm {a.age}</div>
-                                    <button onClick={() => this.props.handleDeleteUser(a.id)} >Delete</button>
-                                    <hr />
-                                </div>
-                            )
-                        })}
-
-                    </>
-                }
-                <img src={logo} />
+    return (
+        <div className="display-info-container">
+            <div>
+                <button onClick={()=>{handleShowHideListUser()}}>
+                {isShowHideListUser===true?"Hide":"Show"}</button>
             </div>
-        )
-    }
+            {isShowHideListUser &&
+                <>
+                    {listUsers.map(a => {
+                        console.log(a)
+                        return (
+                            <div className={+a.age > 18 ? "red" : "green"} key={a.id}>
+                                <div> Hello {a.name}</div>
+                                <div> I'm {a.age}</div>
+                                <button onClick={() => props.handleDeleteUser(a.id)} >Delete</button>
+                                <hr />
+                            </div>
+                        )
+                    })}
+
+                </>
+            }
+            {/* <img src={logo} alt="logo thôi mà"/> */}
+        </div>
+    )
+
 }
 
 export default DisplayInfo
--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -1,60 +1,93 @@
 // class component
 // fucntion component
 
-import React from "react";
+import React, { useState } from "react";
 import DisplayInfo from "./DisplayInfo";
 import AddUserInfo from "./AddUserInfo";
 
-class MyComponent extends React.Component {
-    state = {
-        listUsers:
-            [
-                { id: 1, name: "Nguyen Van A", age: 17 },
-                { id: 2, name: "Nguyen Van b", age: 25 },
-                { id: 3, name: "Nguyen Van c", age: 69 }
-            ]
+const MyComponent = () => {
+    const data =
+        [
+            { id: 1, name: "Nguyen Van A", age: 17 },
+            { id: 2, name: "Nguyen Van b", age: 25 },
+            { id: 3, name: "Nguyen Van c", age: 69 }
+        ]
+    const [listUsers, setListUsers] = useState(data)
 
-    }
-    handleAddNewUser = (user) => {
-        // let listUserClone = [...this.state.listUsers]
-        // listUserClone.unshift(user)
-        // console.log("check",listUserClone)
-        // this.setState({
-        //     listUsers: listUserClone
-        // })
-        this.setState({
-            // listUsers: [ ...this.state.listUsers,user]
-            listUsers: [user, ...this.state.listUsers]
-        })
+
+    const handleAddNewUser = (user) => {
+        const newListUsers = [user, ...listUsers]
+        setListUsers(newListUsers)
     }
 
-    handleDeleteUser = (userId) => {
-        let listUserClone = this.state.listUsers
-        listUserClone = listUserClone.filter(item => item.id !== userId)
-        this.setState({
-            listUsers: listUserClone
-        })
-
+    const handleDeleteUser = (userId) => {
+        const newListUsers = listUsers.filter(a => a.id != userId)
+        setListUsers(newListUsers)
     }
 
-    render() {
-        return (
-            <>
-                <div className="a">
-                    <AddUserInfo
-                        handleAddNewUser={this.handleAddNewUser}
+    return (
+        <>
+            <div className="a">
+                <AddUserInfo handleAddNewUser={handleAddNewUser}
 
-                    // không dùng dấu đóng mở ngoặc khi truyền hàm từ cha sang con, tham chiếu ko cần (), nếu thực thi thì cần
-                    />
-                    <br /><br />
-                    <DisplayInfo handleDeleteUser={this.handleDeleteUser} listUsers={this.state.listUsers} />
-                </div>
-                <div className="b">
+                // không dùng dấu đóng mở ngoặc khi truyền hàm từ cha sang con, tham chiếu ko cần (), nếu thực thi thì cần
+                />
+                <br /><br />
+                <DisplayInfo handleDeleteUser={handleDeleteUser} listUsers={listUsers} />
+            </div>
+            <div className="b">
 
-                </div>
-            </>
-        )
-    }
+            </div>
+        </>
+    )
 }
+
+// class MyComponent extends React.Component {
+//     state = {
+//         listUsers:
+
+
+//     }
+//     handleAddNewUser = (user) => {
+//         // let listUserClone = [...this.state.listUsers]
+//         // listUserClone.unshift(user)
+//         // console.log("check",listUserClone)
+//         // this.setState({
+//         //     listUsers: listUserClone
+//         // })
+//         this.setState({
+//             // listUsers: [ ...this.state.listUsers,user]
+//             listUsers: [user, ...this.state.listUsers]
+//         })
+//     }
+
+//     handleDeleteUser = (userId) => {
+//         let listUserClone = this.state.listUsers
+//         listUserClone = listUserClone.filter(item => item.id !== userId)
+//         this.setState({
+//             listUsers: listUserClone
+//         })
+
+//     }
+
+//     render() {
+//         return (
+//             <>
+//                 <div className="a">
+//                     <AddUserInfo
+//                         handleAddNewUser={this.handleAddNewUser}
+
+//                     // không dùng dấu đóng mở ngoặc khi truyền hàm từ cha sang con, tham chiếu ko cần (), nếu thực thi thì cần
+//                     />
+//                     <br /><br />
+//                     <DisplayInfo handleDeleteUser={this.handleDeleteUser} listUsers={this.state.listUsers} />
+//                 </div>
+//                 <div className="b">
+
+//                 </div>
+//             </>
+//         )
+//     }
+// }
 
 export default MyComponent
