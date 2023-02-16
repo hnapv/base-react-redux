86e7c2a7d4d78fee58762bf1557277b62cb534e9
hnavp <vuphucanh1197@gmail.com>
2023-02-16 14:03:50 +0700

    add reactdom bootrap

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
 import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponents';
-import React from 'react';
 
 
-class App extends React.Component {
-  // const count = useSelector(state => state.counter.count);
-  // const dispatch = useDispatch();
-  render() {
-    return (
-      <div className='app-container'>
-        Hello world
-        <MyComponent />
-      </div>
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <p>
-      //       Edit <code>src/App.js</code> and save to reload.
-      //     </p>
-      //     <div>Count = {count}</div>
-      //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-      //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      //   </header>
-      // </div>
-    );
+const App = ()=>{
 
-  }
+  return (
+    <div className="app-container">
+      Hello world
+
+    </div>
+  );
 }
-
-
 
 export default App;
