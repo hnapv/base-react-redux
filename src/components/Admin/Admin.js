664c9cbe1d09bee7467f082b259e622ccc064edf
hnapv <vuphucanh1197@gmail.com>
2023-02-18 21:54:35 +0700

    add axios api add new user

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -3,8 +3,8 @@ import './Admin.scss';
 import { FaBars } from 'react-icons/fa';
 import { useState } from "react";
 import { Outlet } from 'react-router-dom';
-
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Admin = (props) => {
     const [collapsed, setCollapsed] = useState(false);
@@ -23,6 +23,20 @@ const Admin = (props) => {
                     <Outlet />
                 </div>
             </div>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+            {/* Same as */}
+            <ToastContainer />
         </div>
     )
 }
--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
 
 import { FcPlus } from "react-icons/fc"
 
-const ModalCreateUser = () => {
-    const [show, setShow] = useState(false);
+const ModalCreateUser = (props) => {
+    const { show, setShow } = props
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleClose = () => {
+        setShow(false);
+        setEmail("")
+        setPassword("")
+        setUsername("")
+        setRole("USER")
+        setImage("")
+        setPreviewImage("")
+    }
+    // const handleShow = () => setShow(true);
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
@@ -18,22 +29,55 @@ const ModalCreateUser = () => {
     const [previewImage, setPreviewImage] = useState("")
 
     const handleUploadImage = (event) => {
-        if(event.target && event.target.files && event.target.files[0]){
+        if (event.target && event.target.files && event.target.files[0]) {
             setPreviewImage(URL.createObjectURL(event.target.files[0]))
             setImage(event.target.files[0])
-        }else{
-        // setPreviewImage("")
+        } else {
+            // setPreviewImage("")
+        }
+    }
+
+    const validateEmail = (email) => {
+        return String(email)
+          .toLowerCase()
+          .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          );
+      };
+
+    const handleSubmitCreateUser = async () => {
+        //validate
+        const isValidEmail = validateEmail(email)
+        if(!isValidEmail){
+            toast.error("Invalid email")
+            return;
         }
 
+        if(!password){
+            toast.error("Invalid password")
+            return;
+        }
 
+        const data = new FormData();
+        data.append('email', email);
+        data.append('password', password);
+        data.append('username', username);
+        data.append('role', role);
+        data.append('userImage', image);
+
+        let res = await axios.post('http://localhost:8081/api/v1/participant', data)
+        console.log("res==>", res.data)
+        if(res.data && res.data.EC===0){
+            toast.success(res.data.EM)
+            handleClose()
+        } 
+        if(res.data && res.data.EC!==0){
+            toast.error(res.data.EM)
+        }
     }
 
     return (
         <>
-            <Button variant="primary" onClick={handleShow}>
-                Launch demo modal
-            </Button>
-
             <Modal
                 show={show}
                 onHide={handleClose}
@@ -87,7 +131,7 @@ const ModalCreateUser = () => {
                         </div>
                         <div className='col-md-12'>
                             <label className="form-label label-upload" htmlFor='labelUpload'>
-                                <FcPlus />Upload:
+                                <FcPlus />Upload File Image
                             </label>
                             <input
                                 type="file"
@@ -122,7 +166,7 @@ const ModalCreateUser = () => {
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
+                    <Button variant="primary" onClick={() => handleSubmitCreateUser()}>
                         Save
                     </Button>
                 </Modal.Footer>
